894eeedbbbbe86442e286efff363142aff244b5d
nt4rever <levantanald@gmail.com>
2022-11-07 16:13:02 +0700

    create table payroll

--- src/pages/PayRoll/Payroll.js
+++ src/pages/PayRoll/Payroll.js
@@ -1,7 +1,28 @@
+import { Table } from "antd";
 import React from "react";
+import { columns, expandedRowRender } from "./table";
+import { employees } from "./data/employees";
 
 const Payroll = () => {
-  return <div>Payroll Management</div>;
+  return (
+    <div>
+      <Table
+        rowSelection
+        columns={columns}
+        expandable={{
+          expandedRowRender,
+          defaultExpandedRowKeys: ["0"],
+        }}
+        rowKey="id"
+        dataSource={employees}
+        bordered
+        size="middle"
+        scroll={{
+          y: "calc(100vh - 250px)",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Payroll;
